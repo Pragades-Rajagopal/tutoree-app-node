452fbbf35fe568b9039c6b163c20eeb013e02ab1
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2024-03-25 13:19:55 +0530

    adds console

--- src/connector/database.js
+++ src/connector/database.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const dbPath = path.resolve(__dirname, '../../', 'db.sqlite');
 const appDB = new sqlite.Database(dbPath, sqlite.OPEN_READWRITE, (err) => {
+    console.log('db path:', dbPath);
     if (err) {
         console.log('connection error : ', err.message);
     } else {
