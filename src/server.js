82ae1c69d6b8f3e513937a9db71f0c419f121baf
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2024-05-08 22:53:22 +0530

    adds hostname to server listen

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use('/api', router)
 
-app.listen(PORT, () => { console.log(`App is running > http://${HOSTNAME}:${PORT}`) });
+app.listen(PORT, HOSTNAME, () => { console.log(`App is running > http://${HOSTNAME}:${PORT}`) });
